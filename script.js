6c7acc9725364e5a420c14c22358975ea9961857
DoubiCan <121158263+DoubiCan@users.noreply.github.com>
2025-01-20 22:12:22 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 打开侧边栏
+function openSidebar() {
+    document.getElementById("sidebar").style.left = "0";
+}
+
+// 关闭侧边栏
+function closeSidebar() {
+    document.getElementById("sidebar").style.left = "-250px";
+}
